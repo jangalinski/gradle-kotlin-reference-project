12c255911052b98db72d628c1f49a332e960e064
Jan Galinski <jan.galinski@holisticon.de>
2018-11-25 14:05:41 +0100

    g5 rc5

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,44 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
   `kotlin-dsl`
 }
 
 repositories {
   jcenter()
-}
-
-//rootProject.apply{
-//   from("../gradle/kotlin-project.kts")
-//}
-
-
-subprojects {
-  if (hasSource("kotlin")) {
-    applyKotlinConventions()
-  }
-}
-
-fun Project.hasSource(name:String) = file("src/main/$name").isDirectory || file("src/test/$name").isDirectory
-
-fun Project.applyKotlinConventions() {
-  apply(plugin = "kotlin")
-
-  tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-      jvmTarget = JavaVersion.VERSION_1_8.name
-      freeCompilerArgs += listOf("-Xjsr305=strict")
-    }
-  }
-
-  dependencies {
-    compile(kotlin("stdlib-jdk8"))
-    compile(kotlin("reflect"))
-  }
-
-  plugins.withType<KotlinDslPlugin> {
-    kotlinDslPluginOptions {
-      experimentalWarning.set(false)
-    }
-  }
 }
